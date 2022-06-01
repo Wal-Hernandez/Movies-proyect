6c1dc0aac06f252ba46ba43948e315ca8399b861
WalHernandez <walter.3034@hotmail.com>
2022-06-01 02:37:43 -0300

    token

--- src/utils/httpClient.js
+++ src/utils/httpClient.js
@@ -3,7 +3,7 @@ export function get(path) {
   return fetch(API + path, {
     headers: {
       Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3Yzc0NTI5YWMxMzNlYmFmYzUwYzkxYWQ4NjUzNTBmYyIsInN1YiI6IjYyMWU3YjVmM2FjZDIwMDAxODI3M2ViYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.aD-nmtJlNhqrV3UleP9U9RRLJvFzS0iuVHxADsdpm6o",
+        `Bearer ${process.env.MY_EMAIL}`,
       "Content-Type": "application/json;charset=utf-8",
     },
   }).then((result) => result.json());
